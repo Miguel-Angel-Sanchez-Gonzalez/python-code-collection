183414d445cd35478c33330f0aa41a8c8baf662e
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-02-20 21:50:31 -0600

    feat: Add 'games' directory and Mad Libs game

---
+++ games/mad_libs.py
@@ -0,0 +1,8 @@
+# Mad Libs
+adjetive = input("Adjetivo: ")
+first_verb = input("Verbo: ")
+second_verb = input("Verbo: ")
+noun = input("Sustantivo (plural): ")
+
+print(f"¡Programar es {adjetive}! Siempre me emociona porque me encanta {first_verb} problemas.")
+print(f"¡Aprende a {second_verb} con tu musica favorita y alcanza tus {noun}!")
