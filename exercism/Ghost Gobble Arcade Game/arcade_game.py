d6c4bf5628f76a939c9cd00108feb45dd0cb8970
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-02-22 21:09:44 -0600

    refactor: Remove unnecessary 'else' statements

--- exercism/Ghost Gobble Arcade Game/arcade_game.py
+++ exercism/Ghost Gobble Arcade Game/arcade_game.py
@@ -10,8 +10,7 @@ def eat_ghost(power_pellet_active, touching_ghost):
 
     if power_pellet_active and touching_ghost:
         return True
-    else:
-        return False
+    return False
 
 
 def score(touching_power_pellet, touching_dot):
@@ -23,8 +22,7 @@ def score(touching_power_pellet, touching_dot):
     """
     if touching_power_pellet or touching_dot:
         return True
-    else:
-        return False
+    return False
 
 
 def lose(power_pellet_active, touching_ghost):
@@ -36,8 +34,7 @@ def lose(power_pellet_active, touching_ghost):
     """
     if not power_pellet_active and touching_ghost:
         return True
-    else: 
-        return False
+    return False
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -51,5 +48,4 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
 
     if has_eaten_all_dots and not lose(power_pellet_active, touching_ghost):
         return True
-    else:
-        return False
+    return False
