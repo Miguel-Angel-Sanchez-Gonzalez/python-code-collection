e613a10e782e6c6d80e91062fb392fa1ec581a4a
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-02-24 20:27:22 -0600

    refactor: Remove unnecessary 'elif' statements

--- exercism/Meltdown Mitigation/conditionals.py
+++ exercism/Meltdown Mitigation/conditionals.py
@@ -14,11 +14,7 @@ def is_criticality_balanced(temperature, neutrons_emitted):
     - The product of temperature and neutrons emitted per second is less than 500000.
     """
 
-    if temperature < 800 and neutrons_emitted > 500 and (temperature * neutrons_emitted) < 500000 : 
-        return True
-    return False
-    
-
+    return temperature < 800 and neutrons_emitted > 500 and (temperature * neutrons_emitted) < 500000 
 
 def reactor_efficiency(voltage, current, theoretical_max_power):
     """Assess reactor efficiency zone.
@@ -44,13 +40,12 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
     percentage_value = (generated_power / theoretical_max_power) * 100
     if percentage_value >= 80 :
         return 'green'
-    elif percentage_value < 80 and percentage_value >= 60 :
+    elif percentage_value >= 60 :
         return 'orange'
-    elif percentage_value < 60 and percentage_value >= 30 : 
+    elif percentage_value >= 30 :
         return 'red'
-    elif percentage_value < 30 :
+    else :
         return 'black'
-    
 
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
