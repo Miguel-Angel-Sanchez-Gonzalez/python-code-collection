a3d358e429796b4a03e8645572720faf16873dd1
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-04-21 23:38:37 -0600

    refactor: Eliminate unnecessary documentation comments in lista functions

--- exercism/Card Games/lists.py
+++ exercism/Card Games/lists.py
@@ -1,10 +1,7 @@
+# Solución al ejercicio de Exercism: "Lists"
+# Enunciado tomado de Exercism.org
+
 def get_rounds(number):
-    """Create a list containing the current and next two round numbers.
-
-    :param number: int - current round number.
-    :return: list - current round and the two that follow.
-    """
-
     list_rounds = []
     for i in range(3):
         list_rounds.append(number + i)
@@ -13,13 +10,6 @@ def get_rounds(number):
 # print(get_rounds(27))
 
 def concatenate_rounds(rounds_1, rounds_2):
-    """Concatenate two lists of round numbers.
-
-    :param rounds_1: list - first rounds played.
-    :param rounds_2: list - second set of rounds played.
-    :return: list - all rounds played.
-    """
-
     return rounds_1 + rounds_2
   
 # print(concatenate_rounds([27, 28, 29], [35, 36]))
@@ -27,13 +17,6 @@ def concatenate_rounds(rounds_1, rounds_2):
 
 
 def list_contains_round(rounds, number):
-    """Check if the list of rounds contains the specified number.
-
-    :param rounds: list - rounds played.
-    :param number: int - round number.
-    :return: bool - was the round played?
-    """
-
     return number in rounds
   
   
@@ -42,12 +25,6 @@ def list_contains_round(rounds, number):
 # print(list_contains_round([27, 28, 29, 35, 36], 30))
 
 def card_average(hand):
-    """Calculate and returns the average card value from the list.
-
-    :param hand: list - cards in hand.
-    :return: float - average value of the cards in the hand.
-    """
-
     sum_cards = sum(hand)
     return sum_cards / len(hand)
   
@@ -55,12 +32,6 @@ def card_average(hand):
 
 
 def approx_average_is_average(hand):
-    """Return if the (average of first and last card values) OR ('middle' card) == calculated average.
-
-    :param hand: list - cards in hand.
-    :return: bool - does one of the approximate averages equal the `true average`?
-    """
-
     average = card_average(hand)
     approx_average = (hand[0] + hand[-1]) / 2
     middle_card = hand[len(hand) // 2]
@@ -72,11 +43,6 @@ def approx_average_is_average(hand):
 
 
 def average_even_is_average_odd(hand):
-    """Return if the (average of even indexed card values) == (average of odd indexed card values).
-
-    :param hand: list - cards in hand.
-    :return: bool - are even and odd averages equal?
-    """
 
     even_cards = hand[1::2]
     odd_cards = hand[0::2]
@@ -94,11 +60,6 @@ def average_even_is_average_odd(hand):
 # print(average_even_is_average_odd([1, 2, 3, 4]))
 
 def maybe_double_last(hand):
-    """Multiply a Jack card value in the last index position by 2.
-
-    :param hand: list - cards in hand.
-    :return: list - hand with Jacks (if present) value doubled.
-    """
     last_card = hand[-1]
     if last_card == 11:
         print("entre")
